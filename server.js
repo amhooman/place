4e0012e5014a15754f9935ad3de7282115625aff
Zeus386 <>
2022-04-18 21:51:04 +0000

    Backup

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ setInterval(async function() {
     c.send(buf)
   }
   //if(I % 720 == 0){
-  if (I % 120 == 0) {
+  if (I % 100 == 0) {
     try {
       await pushImage()
       console.log("[" + new Date().toISOString() + "] Successfully saved r/place!")
